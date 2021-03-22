7f71441adc59d062906090d82b2c73e14f1c97c7
LakkyTub <48420418+LakkyTub@users.noreply.github.com>
2021-03-22 19:02:25 +0300

    autoScroll.js

---
+++ autoScroll.js
@@ -0,0 +1,13 @@
+var lastScrollHeight = 0; 
+function autoScroll() { 
+  var sh = document.documentElement.scrollHeight; 
+  if (sh != lastScrollHeight) { 
+    lastScrollHeight = sh; 
+    document.documentElement.scrollTop = sh; 
+  } 
+} 
+window.setInterval(autoScroll, 100); 
+
+
+//TO STOP, CALL:  
+//clearInterval();
